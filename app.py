e222552a30e7d29f692533016ad118f1f4560d2c
Rishabh Nanawati <rishabh.nanawati7@gmail.com>
2021-07-31 18:05:02 +0000

    added login page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # home route
 @app.route("/", methods=['GET'])
 def index():
-    render_template('app.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
